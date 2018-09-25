867ba0fabef7a7e7420be682166db07c1f0c08f0
Thecodingnerd88 <40854335+thecodingnerd88@users.noreply.github.com>
2018-09-25 15:56:01 +0300

    Add files via upload
    Added CSS file.
    Worked on Input change and Reset button functions

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -2,7 +2,9 @@ var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var p1Button = document.getElementById("p1Button");
 var p2Button = document.querySelector("#p2Button");
-var reset = document.querySelector("#Reset");
+var resetButton = document.querySelector("#Reset");
+var winningScoreDisplay = document.querySelector("p span");
+var numInput = document.querySelector("input");
 var gameOver=false;
 var p1Score = 0 ;
 var p2Score = 0 ;
@@ -13,6 +15,7 @@ p1Button.addEventListener("click", function(){
         p1Score++;
         if (p1Score === winningScore){
             gameOver = true;
+            p1Display.classList.add("green");
         }
         p1Display.textContent = p1Score;
     }
@@ -22,12 +25,27 @@ p2Button.addEventListener("click", function(){
         p2Score++;
         if (p2Score === winningScore){
             gameOver=true;
+            p2Display.classList.add("green");
         }
         p2Display.textContent = p2Score;
     }
 });
-reset.addEventListener("click",function(){
+resetButton.addEventListener("click",function(){
+reset();
+});
+
+numInput.addEventListener("change",function(){
+winningScoreDisplay.textContent = numInput.value;
+winningScore = Number(numInput.value);
+reset();
+});
+
+function reset(){
+    p1Score = 0 ;
+    p2Score = 0 ;
     p1Display.textContent=0;
     p2Display.textContent=0;
     gameOver= false;
-});
+    p1Display.classList.remove("green");
+    p2Display.classList.remove("green");
+}
